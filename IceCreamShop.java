f0509cf8f9db3e4f3e04419242f293bd3736443c
kyuthin74 <151596174+kyuthin74@users.noreply.github.com>
2024-04-28 16:13:20 +0000

    28_April

--- IceCreamShop.java
+++ IceCreamShop.java
@@ -249,10 +249,10 @@ public class IceCreamShop {
             // checkSameType();
 
         } else {
-            System.out.print("*********************\nWhat do you want to change? (1.flavor 2.type 3.quantity 4. Delete the item): ");
+            System.out.print("*********************\nWhat do you want to change? (1.flavor 2.type 3.quantity): ");
             String s3 = input.next();
-            while (!s3.equals("1") && !s3.equals("2") && !s3.equals("3")  && !s3.equals("4")) {
-                System.out.println("Choose only 1 to 4: ");
+            while (!s3.equals("1") && !s3.equals("2") && !s3.equals("3")) {
+                System.out.println("Choose only 1 to 3: ");
                 s3 = input.next();
             }
             int etype = Integer.parseInt(s3);
